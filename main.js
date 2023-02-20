df2193c4747f5326570ee87abd8a9cf87b4ea0d8
Robert Schlereth <robert.schlereth@gmail.com>
2023-02-19 20:15:02 -0600

    add build out

---
+++ main.js
@@ -0,0 +1,37 @@
+var recipeInput = document.querySelector("add-recipe");
+
+var addRecipeBtn = document.querySelector("button1");
+
+
+
+button.addEventListener("click", addRecipe);
+
+
+var sideMealInput = document.querySelector("Side");
+var mainDishMealInput = document.querySelector("Main Dish");
+var dessertMealInput = document.querySelector("Dessert");
+var entireMealMealInput = document.querySelector("Entire Meal");
+
+var letsCookBtn = document.querySelector("lets-cook")
+
+button.addEventListener("click", letsCook)
+
+function letsCook() {
+    
+    hideCookpot();
+}
+
+// function meal() {
+//     var meal;
+
+//     if (document.getElementById("Side").checked == true) {
+//         meal = side;}
+//     else if (document.getElementById("Main Dish").checked == true) {
+//         meal = mainDish;}
+//     else if (document.getElementById("Dessert").checked == true) {
+//         meal = dessert;}
+//     else (document.getElementById("Entire Meal").checked == true) {
+//         meal = entireMeal;}
+//     }
+
+
