7af6e46cdecde7e3e7e582a49361f995fe629118
Robert Schlereth <robert.schlereth@gmail.com>
2023-02-19 21:41:29 -0600

    Show how prone I am to falling down rabbit holes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var addRecipeBtn = document.querySelector("button1");
 
 
 
-button.addEventListener("click", addRecipe);
+// button.addEventListener("click", addRecipe);
 
 
 var sideMealInput = document.querySelector("Side");
@@ -13,25 +13,32 @@ var dessertMealInput = document.querySelector("Dessert");
 var entireMealMealInput = document.querySelector("Entire Meal");
 
 var letsCookBtn = document.querySelector("lets-cook")
+console.log("lets-cook")
 
-button.addEventListener("click", letsCook)
-
+letsCookBtn.addEventListener("click", letsCook);
 function letsCook() {
+var meal = letsCook.value
+    heading.innerText = meal
+    if (document.getElementById("Side").checked == true) {
+        meal = side;
+    }
+    else if (document.getElementById("Main Dish").checked == true) {
+        meal = mainDish;
+    }
+    else if (document.getElementById("Dessert").checked == true) {
+        meal = dessert;
+    }
+    else (document.getElementById("Entire Meal").checked == true) 
+        meal = entireMeal;
     
     hideCookpot();
 }
 
-// function meal() {
-//     var meal;
 
-//     if (document.getElementById("Side").checked == true) {
-//         meal = side;}
-//     else if (document.getElementById("Main Dish").checked == true) {
-//         meal = mainDish;}
-//     else if (document.getElementById("Dessert").checked == true) {
-//         meal = dessert;}
-//     else (document.getElementById("Entire Meal").checked == true) {
-//         meal = entireMeal;}
-//     }
+function hideCookpot() {
+    box2.classList.add('hidden')
+}
+
+
 
 
